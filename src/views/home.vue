c7dc67ae9e21c9e6e2aa4972b5cc27dfeff7b645
liuyun <liuyun@sinovoice.com.cn>
2022-01-12 19:40:21 +0800

    feat(test): test lint-staged

---
+++ src/views/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-tag>home</el-tag>
+  </div>
+  <div class="iasd" style="color: red">123</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  props: {},
+  components: {},
+  setup(props) {
+    const asd = 123;
+    return {};
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.iasd {
+  background-color: #fff;
+  color: #fff;
+}
+</style>
