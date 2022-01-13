29357f8a6fe05daf09869f30e1f90a3793066418
liuyun <liuyun@sinovoice.com.cn>
2022-01-13 17:43:50 +0800

    feat(xxx): test
    config cz

--- .cz-config.js
+++ .cz-config.js
@@ -1,65 +1,48 @@
+'use strict';
+
 module.exports = {
+
   types: [
     {
+      value: 'WIP',
+      name : '💪  WIP:      Work in progress'
+    },
+    {
       value: 'feat',
-      name: 'feat: 新功能'
+      name : '✨  feat:     A new feature'
     },
     {
       value: 'fix',
-      name: 'fix: 修复bug'
-    },
-    {
-      value: 'init',
-      name: 'init: 初始化'
-    },
-    {
-      value: ':pencil2: docs',
-      name: 'docs: 文档变更'
-    },
-    {
-      value: 'style',
-      name: 'style: 代码的样式美化'
+      name : '🐞  fix:      A bug fix'
     },
     {
       value: 'refactor',
-      name: 'refactor: 重构'
+      name : '🛠  refactor: A code change that neither fixes a bug nor adds a feature'
     },
     {
-      value: 'perf',
-      name: 'perf: 性能优化'
+      value: 'docs',
+      name : '📚  docs:     Documentation only changes'
     },
     {
       value: 'test',
-      name: 'test: 测试'
-    },
-    {
-      value: 'revert',
-      name: 'revert: 回退'
-    },
-    {
-      value: 'build',
-      name: 'build: 打包'
+      name : '🏁  test:     Add missing tests or correcting existing tests'
     },
     {
       value: 'chore',
-      name: 'chore: 构建/工程依赖/工具'
+      name : '🗯  chore:    Changes that don\'t modify src or test files. Such as updating build tasks, package manager'
     },
     {
-      value: 'ci',
-      name: 'ci: 持续集成的配置文件/脚本修改'
+      value: 'style',
+      name : '💅  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
+    },
+    {
+      value: 'revert',
+      name : '⏪  revert:   Revert to a commit'
     }
   ],
-  messages: {
-    type: '请选择提交类型(必填)',
-    customScope: '请输入文件修改范围(可选)',
-    subject: '请简要描述提交(必填)',
-    body: '请输入详细描述(可选)',
-    breaking: '列出任何BREAKING CHANGES(可选)',
-    footer: '请输入要关闭的issue(可选)',
-    confirmCommit: '确定提交此说明吗？'
-  },
-  allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix'], // 当提交类型为feat、fix时才有破坏性修改选项
-  subjectLimit: 100
-};
 
+  scopes: [],
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ["feat", "fix"]
+};
