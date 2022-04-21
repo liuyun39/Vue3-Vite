a224d71c6c99902b7172a6ce2c0e59fc287c2790
liuyun <2893699068@QQ.com>
2022-04-21 23:45:55 +0800

    feat(自定义播放条)： 自定义播放条

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,110 +1,84 @@
 <template>
-  <div>
-    <button @click="start">
-      开始录音
-    </button>
-    <button @click="stop">
-      结束录音
-    </button>
-    <button @click="bofang">
-      录音播放
-    </button>
-    <button @click="handleData">
-      获取数据
-    </button>
-    <div>{{ blobsData }}</div>
-  </div>
+    <div class="audio-content">
+        <div class="audio-left">
+            <el-switch
+                v-model="playing" />
+            <div class="audio-progress">
+                <el-slider
+                    v-model="currentTime"
+                    :format-tooltip="formatTooltip"
+                    :max="duration"
+                    style="width: 200px" />
+            </div>
+        </div>
+        <div class="audio-title">
+            {{ playing ? '音乐播放中。。。' : '未播放音乐。。。' }}
+        </div>
+        <audio ref="audioRef" />
+    </div>
 </template>
+
 <script lang="ts">
-import {
-	defineComponent,
-	nextTick,
-	onMounted,
-	reactive,
-	ref,
-} from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import { useMediaControls } from "@vueuse/core";
 
-import { ElForm, ElMessageBox, ElMessage } from "element-plus";
-import Recorder from "js-audio-recorder";
-
-export default defineComponent({
-	name: "",
-	components:{
+export default defineComponent ({
+	components: { 
 	},
-	setup (props) {
-		const recorder = new Recorder({
-			sampleBits: 16,                 // 采样位数，支持 8 或 16，默认是16
-			sampleRate: 16000,              // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
-			numChannels: 1,                 // 声道，支持 1 或 2， 默认是1
-			// compiling: false,(0.x版本中生效,1.x增加中)  // 是否边录边转换，默认是false
+	setup () {
+		const audioRef = ref<HTMLAudioElement>();
+		const { playing, currentTime, duration, volume } = useMediaControls( audioRef, { 
+			src: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
 		});
 
-		console.log(recorder);
+		const totalTime = ref(0);
+		onMounted(() => {
+			totalTime.value = duration.value;
+		});
 
-		// onMounted(() => {
-		// 	quanxian();
-		// });
-
-		// 开始录音
-		function start () {
-			quanxian();
-			recorder.start().then(() => {
-				// 开始录音
-				console.log("开始录音");
-
-			}, (error) => {
-				// 出错了
-				console.log(error);
-			});
+		function formatTooltip (v: number) {
+			return `${addZero((v / 60).toFixed(0))}:${addZero((v % 60).toFixed(0))}`;
 		}
 
-		recorder.onprogress = (res: any) => {
-			console.log("音量", res.vol);
-			console.log("blob", res.data);
-
-		};
-
-		// 关闭录音
-		function stop () {
-			//
-			recorder.stop();
+		function addZero (v: string) {
+			return parseFloat(v) <= 9 ? `0${v}` : v;
 		}
-
-		// 播放录音
-		function bofang () {
-			recorder.play();
-		}
-
-		// 权限
-		function quanxian () {
-			(Recorder as any).getPermission().then(() => {
-				console.log("给权限了");
-			}, (error: any) => {
-				console.log(`${error.name} : ${error.message}`);
-			});
-		}
-
-		// handleData
-		const blobsData: any = ref(null);
-		function handleData () {
-			//
-			blobsData.value = recorder.getWAVBlob();
-			console.log(blobsData.value, 123);
-
-		}
-
-
 		return {
-			start,
-			stop,
-			bofang,
-			handleData,
-			blobsData
+			playing,
+			currentTime,
+			duration,
+			totalTime,
+			audioRef,
+
+			formatTooltip
 		};
 	}
 });
-</script>
+</script> 
 
 <style lang="scss" scoped>
+.audio-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 600px;
+    margin: auto;
+    padding: 0 12px;
+    height: 50px;
+    background-color: #fff;
+    border-radius: 5px;
+    color: #000;
 
+    .audio-left {
+        display: flex;
+        .audio-progress {
+            margin-left: 12px;
+        }
+    }
+    .audio-title {
+        display: flex;
+        justify-content: start;
+    }
+
+}
 </style>
